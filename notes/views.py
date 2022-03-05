47b1a008916a9c65a940644b496718e054ed8b0c
Ryan Urbanski <ryanurbanski9@gmail.com>
2022-03-05 18:57:44 -0500

    Section 4 Completed

--- notes/views.py
+++ notes/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import Http404
+from django.views.generic import DetailView, ListView
 
 from .models import Notes
 
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = "notes"
+    template_name = 'notes/notes_list.html'
 
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, 'notes/notes_list.html', {'notes': all_notes})
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
 
 def detail(request, pk):
     try:
